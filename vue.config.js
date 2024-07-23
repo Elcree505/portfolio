03eb089502105fddf663d89ad0f410e241021aeb
Izzel418 <158096821+Elcree505@users.noreply.github.com>
2024-07-22 18:33:51 -0700

    error fixed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfoloio/" : "/",
 };
